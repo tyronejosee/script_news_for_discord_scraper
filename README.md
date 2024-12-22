4a05afa664d1ef2c825e9ca81c274d141dd85a58
tyronejose <alt.tyronejose@gmail.com>
2024-12-22 17:58:14 -0300

    refactor: update main script, add supabase integration, update action workflow

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Script News for Discord Scraper
+
+## Create a Webhook in Discord
+
+A Webhook is a URL that you can use to send messages to a Discord channel. To create a Webhook, follow these steps:
+
+1. Go to your Discord server.
+2. Right-click on the text channel where you want the messages to be posted.
+3. Click on "Channel Settings" (gear icon).
+4. In the left menu, select "Integrations".
+5. Then, select "Webhooks".
+6. Click the "Create Webhook" button.
+7. Configure the Webhook:
+   - Give it a name.
+   - Make sure to select the correct channel where the messages will be posted.
+   - Copy the Webhook URL, which will be something like `https://discord.com/api/webhooks/ID/TOKEN`.
+8. Click Save.
+
+## Create the table in `Supabase`
+
+1. Go to the Supabase dashboard.
+2. Open the project.
+3. Go to the SQL Editor section and run a query to create the table. Use the following SQL:
+
+```sql
+CREATE TABLE IF NOT EXISTS scraped_data (
+    id SERIAL PRIMARY KEY,
+    url TEXT UNIQUE NOT NULL
+);
+```
+
+> This will create the `scraped_data` table with an `id` column as the primary key and a `url` column, unique, to store the URLs.
--- main.py
+++ main.py
@@ -5,35 +5,58 @@ News for Discord Scraper
 import json
 import os
 import logging
-
+import time
 import requests
+
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from supabase import create_client, Client
+
 from logging_configs import setup_logging
 
 load_dotenv()
 setup_logging()
 
+
 WEBHOOK_URL: str = os.environ.get("WEBHOOK_URL")
 if WEBHOOK_URL is None:
     raise ValueError("WEBHOOK_URL environment variable is not set.")
 
-DATA_FILE = "./data/scraped_data.json"
-if DATA_FILE is None:
-    raise ValueError("DATA_FILE environment variable is not set.")
+# Supabase credentials
+SUPABASE_URL: str = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY: str = os.environ.get("SUPABASE_KEY")
+if SUPABASE_URL is None or SUPABASE_KEY is None:
+    raise ValueError("Supabase credentials not set in environment variables.")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 def load_persisted_data():
-    """Loads the persisted data from the file."""
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "r") as file:
-            return json.load(file)
-    return []
+    """Loads the persisted data from Supabase."""
+    try:
+        response = supabase.table("scraped_data").select("*").execute()
+        return [record["url"] for record in response.data]
+    except Exception as e:
+        logging.error(f"Error loading data from Supabase: {e}")
+        return []
 
 
 def save_data(new_data):
-    """Saves the new data to the file."""
-    with open(DATA_FILE, "w") as file:
-        json.dump(new_data, file)
+    """Saves the new data to Supabase."""
+    try:
+        for item in new_data:
+            # Check if URL already exists in Supabase to avoid duplicates
+            existing = (
+                supabase.table("scraped_data").select("url").eq("url", item).execute()
+            )
+            if existing.data:
+                logging.info(f"Duplicate URL found, skipping: {item}")
+                continue
+            # Insert new data
+            supabase.table("scraped_data").insert({"url": item}).execute()
+        logging.info("New data saved successfully.")
+    except Exception as e:
+        logging.error(f"Error saving data to Supabase: {e}")
 
 
 def send_to_discord(message):
@@ -51,20 +74,36 @@ def send_to_discord(message):
     # Check if the request was successful
     if response.status_code == 204:
         logging.info(f"Message successfully sent: {message}")
+    elif response.status_code == 429:
+        logging.error("Rate limit reached.")
     else:
         logging.error(f"Error sending message: {response.status_code}")
+
+
+def scrape_lacuarta():
+    url = "https://www.lacuarta.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    divs = soup.find_all("div", class_="story-card-image article-image")
+
+    links = []
+    for div in divs:
+        a_tag = div.find("a", href=True)
+        if a_tag:
+            link = a_tag["href"]
+            if not link.startswith("http"):
+                link = f"https://www.lacuarta.com{link}"
+            links.append(link)
+
+    return links
 
 
 def scrape_and_send():
     # Load the already persisted data
     existing_data = load_persisted_data()
-
-    # Simulating new scraped data
-    scraped_data = [
-        "https://www.emol.com/noticias/Nacional/2024/12/19/1151792/frente-amplio-volanteo-masivo-reforma.html",
-        "https://www.emol.com/noticias/Nacional/2024/12/21/1152083/fuero-parlamentario-congresistas-investigados.html",
-        "https://www.emol.com/noticias/Nacional/2024/12/21/1152093/lactante-muere-atacada-pitbull-maule.html",
-    ]
+    scraped_data = []
+    scraped_data.extend(scrape_lacuarta())
 
     # Filter new data that is not in the persisted list
     new_data = [item for item in scraped_data if item not in existing_data]
@@ -73,9 +112,10 @@ def scrape_and_send():
         # Send to Discord
         for item in new_data:
             send_to_discord(item)
+            time.sleep(1)
 
         # Save the new persisted data
-        save_data(existing_data + new_data)
+        save_data(new_data)
     else:
         logging.error("No new data to send.")
 
